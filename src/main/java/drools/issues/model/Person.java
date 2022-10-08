b1e801489b13544781870ec39adaf3d3f63d5ef4
Francisco A. Lozano <flozano@gmail.com>
2022-10-08 13:39:14 +0200

    Add overloading methods tests
    Issue is https://issues.redhat.com/browse/DROOLS-7196

--- src/main/java/drools/issues/model/Person.java
+++ src/main/java/drools/issues/model/Person.java
@@ -1,5 +1,11 @@
 package drools.issues.model;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Sets;
+
 public class Person {
 
 	private String firstName;
@@ -9,6 +15,8 @@ public class Person {
 	private final Counter visits = new Counter();
 
 	private final Address address = new Address();
+
+	private Collection<Long> favoriteNumbers = Collections.emptyList();
 
 	public Counter getVisits() {
 		return visits;
@@ -40,6 +48,23 @@ public class Person {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public Collection<Long> getFavoriteNumbers() {
+		return favoriteNumbers;
+	}
+
+	public void setFavoriteNumbers(Collection<Long> favoriteNumbers) {
+		this.favoriteNumbers = favoriteNumbers;
+	}
+
+	public void setFavoriteNumbers(Long... values) {
+		this.favoriteNumbers = values == null ? null : Sets.newHashSet(values);
+	}
+
+	public void setFavoriteNumbers(String... values) {
+		this.favoriteNumbers = values == null ? null
+				: Sets.newHashSet(values).stream().map(Long::parseLong).collect(Collectors.toSet());
 	}
 
 	public static class Address {
---
+++ src/test/java/drools/issues/OverloadNumberMethodTest.java
@@ -0,0 +1,31 @@
+package drools.issues;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import drools.issues.executor.RuntimeType;
+import drools.issues.model.Person;
+
+public class OverloadNumberMethodTest extends AbstractTest {
+
+	@ParameterizedTest
+	@EnumSource(RuntimeType.class)
+	public void arrayOfIntToLongs(RuntimeType runtimeType) {
+		try (var executor = executor(runtimeType, "OverloadNumberMethodTest_arrayOfIntToLongs.drl")) {
+			var fact = fact();
+			var result = executor.fire(fact);
+			assertEquals(1, result.getNumberOfFiredRules());
+			assertEquals(Set.of(7l, 77l, 777l), fact.getFavoriteNumbers());
+		}
+	}
+
+	private static Person fact() {
+		var fact = TestData.person();
+		fact.setFirstName("Pepe");
+		return fact;
+	}
+}
