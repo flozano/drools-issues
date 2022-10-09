1e94562c2de0110b69cb30069bdc2eb865172c06
Francisco A. Lozano <flozano@gmail.com>
2022-10-09 18:20:54 +0200

    Reproduce cast to string differences between mvel and executable model

--- src/main/java/drools/issues/model/SendRegards.java
+++ src/main/java/drools/issues/model/SendRegards.java
@@ -1,7 +1,12 @@
 package drools.issues.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class SendRegards {
 	private String message;
+	private final Map<String, String> params = new HashMap<>();
 	private boolean send = true;
 
 	public SendRegards(String message) {
@@ -23,5 +28,14 @@ public class SendRegards {
 
 	public void setSend(boolean send) {
 		this.send = send;
+	}
+
+	public SendRegards withParam(String name, String value) {
+		params.put(name, Objects.requireNonNull(value));
+		return this;
+	}
+
+	public String getParam(String name) {
+		return params.get(name);
 	}
 }
---
+++ src/test/java/drools/issues/CastTest.java
@@ -0,0 +1,30 @@
+package drools.issues;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import drools.issues.executor.RuntimeType;
+import drools.issues.model.RegardsManager;
+import drools.issues.model.SendRegards;
+import drools.issues.model.vehicles.ElectricCar;
+
+class CastTest extends AbstractTest {
+
+	@ParameterizedTest
+	@EnumSource(RuntimeType.class)
+	public void intToString(RuntimeType runtimeType) {
+		try (var executor = executor(runtimeType, "CastTest_intToString.drl")) {
+			executor.addGlobalVariable("regardsManager", new RegardsManager());
+			var teslaModel3 = new ElectricCar("Tesla", "Model 3", 200, 90);
+			teslaModel3.setScore(10);
+			var result = executor.fire(teslaModel3);
+			assertEquals(1, result.getNumberOfFiredRules());
+			var regards = result.getSingleOutput(SendRegards.class);
+			assertNotNull(regards);
+			assertEquals("10", regards.getParam("score"));
+		}
+	}
+}
